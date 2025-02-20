bf309827261924b49db5719524d2680ad6fb45fa
Lokesh Anna <harshitanna15@gmail.com>
2025-02-20 15:21:21 +0530

    added the changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles/header.css'
 
 const Header = ({ user, onLogout }) => {
+  const navigate = useNavigate();
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary w-100">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">Win Wire</a>
+        <Link className="navbar-brand" to={user ? "/dashboard" : "/login"}>
+          Win Wire
+        </Link>
 
         <button 
           className="navbar-toggler" 
@@ -25,14 +30,14 @@ const Header = ({ user, onLogout }) => {
                   <span className="nav-link text-white">{user.username}</span>
                 </li>
                 <li className="nav-item">
-                  <button className="btn btn-danger ms-2" onClick={onLogout}>
+                  <button className="btn btn-danger ms-2" onClick={() => { onLogout(); navigate("/login"); }}>
                     Logout
                   </button>
                 </li>
               </>
             ) : (
               <li className="nav-item">
-                <a className="nav-link" href="/login">Login</a>
+                <Link className="nav-link" to="/login">Login</Link>
               </li>
             )}
           </ul>
